39254410f62ba20dda137d8dab0bfd4fd06bbc11
CatarinaGimenes <catarina.pires@sptech.school>
2025-04-26 18:47:13 -0300

    Banco de Dados SoyTrack atualizado

--- TabelaSOYTRACK.sql
+++ TabelaSOYTRACK.sql
@@ -11,7 +11,13 @@ token char(8) unique,
 nome varchar(45),
 cnpj char(14),
 email varchar(45),
-telefone char(11)
+telefone char(11),
+logradouro varchar(45),
+num varchar(5),
+bairro varchar(45),
+cidade varchar(45),
+UF char(2),
+CEP char(8)
 );
 
 create table funcionario(
@@ -30,8 +36,8 @@ constraint FKempresafuncionario foreign key (FKempresaf)
 
 create table silo(
 idSilo int primary key auto_increment,
-altura int,
-diametro int, 
+altura float,
+diametro float, 
 FKempresa int,
 constraint FKempresasilo foreign key (FKempresa)
 	references empresa(idEmpresa)
@@ -40,7 +46,7 @@ constraint FKempresasilo foreign key (FKempresa)
 create table sensor(
 idSensor int primary key auto_increment,
 nome varchar(10),
-distanciaMAX int,
+distanciaMAX float,
 FKsilo int unique,
 constraint FKsilosensor foreign key (FKsilo)
 	references silo(idSilo)
@@ -48,8 +54,8 @@ constraint FKsilosensor foreign key (FKsilo)
 
 create table registro(
 idRegistro int auto_increment,
-distancia int,
-dtRegistro datetime,
+distancia decimal(5,2),
+dtRegistro datetime default current_timestamp,
 temIrregularidade boolean,
 FKsensor int,
 constraint PKfkidREGISTRO primary key (idRegistro, FKsensor),
@@ -60,11 +66,8 @@ constraint FKsensorregistro foreign key (FKsensor)
 
 -- INSERTS
 
-insert into empresa (token, nome, cnpj, email, telefone) values
-('58173902', 'Sementec', '98765432000110', 'sementec@email.com', '11983472651'),
-('37492018', 'Kepler Weber', '12345678000190', 'keplerweber@email.com', '11976543820'),
-('92038476', 'Belagrícola', '45678912000137', 'belagricola@email.com', '11998237465'),
-('80367241', 'Scheffer', '23109874000158', 'scheffer@email.com', '11987654321');
+insert into empresa (token, nome, cnpj, email, telefone, logradouro, num, bairro, cidade, UF, CEP) values
+('58173902', 'Sementec', '98765432000110', 'sementec@email.com', '11983472651',  'Rua das Sementes', '45', 'Bairro 123', 'Barreiras', 'BA', '02138879');
 
 -- Inserindo as informações da empresa SEMENTEC
 
@@ -76,7 +79,7 @@ insert into funcionario (nivel, nome, email, senha, FKempresaf) values
 
 
 insert into silo(altura, diametro, FKempresa) values
-(7, 5, 1),
+(7, 7, 1),
 (7, 5, 1),
 (7, 5, 1);
 
@@ -84,17 +87,6 @@ insert into sensor (nome, distanciaMAX, FKsilo) values
 ('HC-SR04', 1, 1),
 ('HC-SR04', 1, 2),
 ('HC-SR04', 1, 3);
-
--- Informações do primeiro sensor da empresa SEMENTEC
-insert into registro (distancia, dtRegistro, temIrregularidade, FKsensor) values
-(7, '2025-04-06 14:00:00', false, 1),
-(6, '2025-04-06 15:00:00', false, 1),
-(5, '2025-04-06 16:00:00', false, 1),
-(4, '2025-04-06 17:00:00', false, 1),
-(3, '2025-04-06 18:00:00', false, 1),
-(2, '2025-04-06 19:00:00', false, 1),
-(1, '2025-04-06 20:00:00', true, 1);
-
 
 -- SELECTS
 
@@ -107,29 +99,6 @@ select empresa.nome as Empresa,
 			on idEmpresa = FKempresaf
 		where idEmpresa = 1;
         
-select empresa.nome as Empresa,
-		silo.idSilo as Silo,
-        sensor.idSensor as Sensor,
-        r.distancia as Nível,
-        r.dtRegistro as Horário,
-        case when r.temIrregularidade = 1 then 'Irregular'
-        else 'Regular' end as 'Irregularidades'
-        from empresa join silo 
-			on idEmpresa = FKempresa
-		join sensor on idSilo = FKsilo
-        join registro as r on idSensor = FKsensor where empresa.idEmpresa = 1 and silo.idSilo = 1;
-        
-/* select  r.distancia as 'Nível dos grãos do silo 1: SEMENTEC',
-        r.dtRegistro as Horário,
-        case when r.temIrregularidade = 1 then 'Irregular'
-        else 'Regular' end as 'Irregularidades'
-        from empresa join silo 
-        on idEmpresa = FKempresa
-        join sensor 
-        on idSilo = FKsilo
-        join registro as r
-        on idSensor = FKsensor
-        where empresa.idEmpresa = 1 and silo.idSilo = 1; */
         
 select  concat(7 - r.distancia, 'm') as 'Nível dos grãos do silo 1: SEMENTEC',
         r.dtRegistro as Horário,
